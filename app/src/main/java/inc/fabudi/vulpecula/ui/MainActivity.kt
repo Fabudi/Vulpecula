944424b4da9727831249fc5be9dd0ae8cab20a89
Fabudi <evgeniydodg@gmail.com>
2023-07-17 02:02:19 +0300

    Remove appbar & made custom

--- app/src/main/java/inc/fabudi/vulpecula/ui/MainActivity.kt
+++ app/src/main/java/inc/fabudi/vulpecula/ui/MainActivity.kt
@@ -3,16 +3,11 @@ package inc.fabudi.vulpecula.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import inc.fabudi.vulpecula.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     lateinit var navController: NavController
 
@@ -22,17 +17,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.mainFragment, R.id.loginFragment
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_container)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
